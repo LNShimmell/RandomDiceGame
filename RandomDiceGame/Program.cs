1f4f0538a0e88306ceec0ce8413f0c35317b2bd7
Larry Shimmell <larryandshimmell@gmail.com>
2018-05-31 17:27:33 -0700

    Check out the Dice Game! Keep adding up the rolls of your die. Be carefull though, if you roll a one, game overgit add .

--- RandomDiceGame/Program.cs
+++ RandomDiceGame/Program.cs
@@ -9,33 +9,33 @@ namespace RandomDiceGame
 {
 	class Program
 	{
-		 Random roll = new Random();
+		 Random roll = new Random();// Random class is called to create a new random object roll
 
 
-		 int RNG()
+		 int RNG()//Method 
 		{
-			return roll.Next(1, 7);
+			return roll.Next(1, 7);//adjusting these attributes changes the type of die example 1,5 would be a four sided die, 1,13 would be a twelve and so on
 		}
 
 		 void Game(){
-			int die = RNG();
-			long Crolls = 0;
-			int score = 0;
-			int MaxScore = 0;
-			long games = 1;
+			int die = RNG();//the die score from each throw is then stored into 'die'
+			long Crolls = 0;// counts the number of throws the game has completed
+			int score = 0; // keeps track of the current score
+			int MaxScore = 0;// keeps track of the highest score while the game is running.
+			long games = 1;// keeps track of how many games the program has executed
 			
 		
 
-			while (score < 1000){
+			while (score < 1000){//Change this value to make the game quit sooner. The likely hood of reaching 1000 without a super computer is almost zero
 				if(die != 1)
 				{
 					score += die;
 					Crolls += 1;
 
 				}
-				if(die == 1)
+				if(die == 1)// if the program rolls  1
 				{
-					score *= 0;
+					score *= 0;//score will become zero
 					Crolls += 1;
 					games += 1;
 				}
@@ -46,10 +46,9 @@ namespace RandomDiceGame
 					Console.WriteLine("High Score: {0}\nRolls: {1}\nGames: {2}\n------------------------------", MaxScore, string.Format("{0:n0}", Crolls), string.Format("{0:n0}", games));
 					
 				}
-				if (Crolls%100000000==0)
+				if (Crolls%100000000==0)// will only display the next line
 				{
 					
-					score += die;
 					Console.WriteLine("High Score: {0}\nRolls: {1}\nGames: {2}\n------------------------------", MaxScore, string.Format("{0:n0}", Crolls), string.Format("{0:n0}", games));
 					
 				}
@@ -68,7 +67,7 @@ namespace RandomDiceGame
 
 		static void Main(string[] args)
 		{
-			(new Program()).run();
+			(new Program()).run();//  Starts up Game method
 		}
 	}
 }
